de4bcb9234ea47af649576979840827743fcac1c
marcus.titze <marcus.titze@localhost>
2008-02-18 21:43:57 +0000

    export to excel

--- django-twa/src/twa/members/views.py
+++ django-twa/src/twa/members/views.py
@@ -8,6 +8,7 @@ from django.db import transaction
 from django.db.models import Q
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response, get_object_or_404
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.simple import direct_to_template, redirect_to
 from twa.members.forms import LoginForm
@@ -255,3 +256,110 @@ def dojos_csv( request ):
         writer.writerow( [str( d.id ), d.name, d.street, d.zip, d.city, d.country.get_name()] )
 
     return response
+
+@login_required
+def members_xls( request ):
+    from pyExcelerator import *
+    wb = Workbook()
+    ws0 = wb.add_sheet( 'Members' )
+
+    ws0.write( 0, 0, 'id')
+    ws0.write( 0, 1, 'firstname')
+    ws0.write( 0, 2, 'lastname')
+    ws0.write( 0, 3, 'street')
+    ws0.write( 0, 4, 'zip')
+    ws0.write( 0, 5, 'city')
+    ws0.write( 0, 6, 'country')
+    ws0.write( 0, 7, 'phone')
+    ws0.write( 0, 8, 'fax')
+    ws0.write( 0, 9, 'mobile')
+    ws0.write( 0, 10, 'email')
+    ws0.write( 0, 11, 'website')
+    ws0.write( 0, 12, 'rank')
+    ws0.write( 0, 13, 'gender')
+    ws0.write( 0, 14, 'birth')
+    ws0.write( 0, 15, 'photo')
+    ws0.write( 0, 16, 'text')
+
+    x = 0
+    for person in Person.persons.all().order_by( 'id' ):
+        x += 1
+        ws0.write( x, 0, str( person.id ) )
+        ws0.write( x, 1, person.firstname )
+        ws0.write( x, 2, person.lastname )
+        ws0.write( x, 3, person.street )
+        ws0.write( x, 4, person.zip )
+        ws0.write( x, 5, person.city )
+        ws0.write( x, 6, __get_name( person.country ) )
+        ws0.write( x, 7, person.phone )
+        ws0.write( x, 8, person.fax )
+        ws0.write( x, 9, person.mobile )
+        ws0.write( x, 10, person.email )
+        ws0.write( x, 11, person.website )
+        ws0.write( x, 12, person.get_current_rank_display() )
+        ws0.write( x, 13, person.get_gender_display() )
+        ws0.write( x, 14, str( person.birth ) )
+        ws0.write( x, 15, person.photo )
+        ws0.write( x, 16, person.text )
+
+    filename = 'members-%s.xls' % datetime.now().strftime( '%Y%m%d%H%M%S' )
+    wb.save( filename )
+    response = HttpResponse( open( filename, 'r' ).read(), mimetype='application/ms-excel' )
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+
+    return response
+
+@login_required
+def members_csv( request ):
+    response = HttpResponse( mimetype='text/csv' )
+    response['Content-Disposition'] = 'attachment; filename=members.csv'
+
+    from csvutf8 import UnicodeWriter
+    writer = UnicodeWriter( response )
+
+    writer.writerow( ['id',
+                      'firstname',
+                      'lastname',
+                      'street',
+                      'zip',
+                      'city',
+                      'country',
+                      'phone',
+                      'fax',
+                      'mobile',
+                      'email',
+                      'website',
+                      'rank',
+                      'gender',
+                      'birth',
+                      'photo',
+                      'text',
+                      ] )
+
+    for person in Person.persons.all().order_by( 'id' ):
+        writer.writerow( [str( person.id ),
+                          person.firstname,
+                          person.lastname,
+                          person.street,
+                          person.zip,
+                          person.city,
+                          __get_name( person.country ),
+                          person.phone,
+                          person.fax,
+                          person.mobile,
+                          person.email,
+                          person.website,
+                          person.get_current_rank_display(),
+                          person.get_gender_display(),
+                          str( person.birth ),
+                          person.photo,
+                          person.text,
+                          ] )
+
+    return response
+
+def __get_name( o ):
+    if o:
+        return o.get_name()
+    else:
+        return ''
