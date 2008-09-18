e7576cabcd25d2cfa7a73cd4309a1032eba2b61e
marcus.titze <marcus.titze@localhost>
2008-09-18 19:17:21 +0000

    admin autodiscover

--- django-twa/src/twa/urls.py
+++ django-twa/src/twa/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns( '',
     # Admin
     ( r'^admin/(.*)', admin.site.root ),
