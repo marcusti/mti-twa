6bb6a663983f8b0483e72541dcec11517ce502e8
marcus.titze <marcus.titze@localhost>
2009-01-20 10:59:21 +0000

    ie6 fix, requests admin filter

--- django-twa/src/twa/requests/admin.py
+++ django-twa/src/twa/requests/admin.py
@@ -7,5 +7,8 @@ class RequestAdmin( admin.ModelAdmin):
     ordering = [ '-created' ]
     list_display = ( 'user_agent', 'user', 'path', 'remote', 'created' )
     list_display_links = ( 'user_agent', 'user' )
-    
+    list_filter = [ 'user' ]
+    date_hierarchy = 'created'
+    search_fields = [ 'user_agent', 'user', 'path' ]
+   
 admin.site.register( Request, RequestAdmin )
