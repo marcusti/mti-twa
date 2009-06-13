9458710f8ac8be230390d7dade8ff664bf0c58ce
marcus.titze <marcus.titze@localhost>
2009-06-13 22:19:52 +0000

    user agents top ten refactoring

--- django-twa/src/twa/requests/models.py
+++ django-twa/src/twa/requests/models.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-from django.db import models
+from django.db import connection, models
 from django.utils.translation import ugettext_lazy as _
 from twa.utils import DEFAULT_MAX_LENGTH, AbstractModel
 
@@ -8,15 +8,20 @@ class RequestManager( models.Manager ):
     def get_query_set( self ):
         return super( RequestManager, self ).get_query_set()
 
-    def get_user_agents( self ):
-        return self.get_query_set().values( 'user_agent' ).distinct()
-
-    def get_user_agents_by_requests( self ):
+    def get_user_agents_top_10( self ):
+        SQL = 'select count(*) as c, user_agent from requests_request group by user_agent order by c desc limit 10'
+        cursor = connection.cursor()
+        cursor.execute( SQL )
         agents = []
-        for ua in self.get_user_agents():
-            agents.append( { 'count': self.get_query_set().filter( user_agent = ua['user_agent'] ).count(), 'user_agent': ua['user_agent'] } )
-        #agents.append( { 'user_agent': '[all requests]', 'count': self.get_query_set().count() } )
+        for i in range( cursor.rowcount ):
+            row = cursor.fetchone()
+            agents.append( { 'count': row[0], 'user_agent': row[1] } )
+        cursor.close()
         return agents
+
+#        Django 1.1
+#        from django.db.models import Count
+#        return self.get_query_set().values( 'user_agent' ).annotate( agent_count = Count( 'user_agent' ) ).order_by( '-agent_count' )[:10]
 
 class Request( AbstractModel ):
     user = models.CharField( _( 'User' ), max_length = DEFAULT_MAX_LENGTH, blank = True )
