f5a5de30a54c5d0b99e3c65508a905079ff7bb9d
marcus.titze <marcus.titze@localhost>
2009-07-28 20:20:55 +0000

    thumbnail filter

--- django-twa/src/twa/members/templatetags/twa_tags.py
+++ django-twa/src/twa/members/templatetags/twa_tags.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django import template
 from django.template.defaultfilters import stringfilter
 import locale
@@ -54,4 +55,28 @@ def paginator( context, adjacent_pages = 3 ):
         'show_last': context['pages'] not in page_numbers,
     }
 
+def thumbnail( file, size = '64x64' ):
+    # defining the size
+    x, y = [int( x ) for x in size.split( 'x' )]
+    # defining the filename and the miniature filename
+    filehead, filetail = os.path.split( file.path )
+    basename, format = os.path.splitext( filetail )
+    miniature = basename + '_' + size + format
+    filename = file.path
+    miniature_filename = os.path.join( filehead, miniature )
+    filehead, filetail = os.path.split( file.url )
+    miniature_url = filehead + '/' + miniature
+    if os.path.exists( miniature_filename ) and os.path.getmtime( filename ) > os.path.getmtime( miniature_filename ):
+        os.unlink( miniature_filename )
+    # if the image wasn't already resized, resize it
+    if not os.path.exists( miniature_filename ):
+        image = Image.open( filename )
+        image.thumbnail( [x, y], Image.ANTIALIAS )
+        try:
+            image.save( miniature_filename, image.format, quality = 90, optimize = 1 )
+        except:
+            image.save( miniature_filename, image.format, quality = 90 )
+    return miniature_url
+
+register.filter( thumbnail )
 register.inclusion_tag( 'paginator.html', takes_context = True )( paginator )
