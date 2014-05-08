791325eb320729d129dbd9987ed8dfcf8eebc19d
Marcus Titze <marcus.titze@gmail.com>
2014-05-08 12:26:00 +0900

    sql statements

--- twa.sql
+++ twa.sql
@@ -46,9 +46,11 @@ SELECT * FROM members_twapayment y WHERE y.year IS NULL;
 
 -- find twa IDs over 2000
 SELECT
-  c.code, m.twa_id_number, p.firstname, p.lastname
+  c.code, m.twa_id_number, m.status, m.passport_date as passport, p.firstname, p.lastname, d.name as dojo
   FROM members_twamembership m
   JOIN members_person p ON p.id=m.person_id
+  JOIN members_person_dojos pd on pd.person_id=p.id
+  JOIN members_dojo d on d.id=pd.dojo_id
   JOIN members_country c ON m.twa_id_country_id=c.id
   WHERE m.twa_id_number >= 2000
   ORDER BY m.twa_id_number DESC nulls last, m.last_modified DESC
